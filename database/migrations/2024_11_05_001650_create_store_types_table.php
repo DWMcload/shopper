66fe22b293947ef31a023dd332c613ded0bc7ab6
Daniel W. Mcload <mcload@alomvilag.hu>
2024-11-06 00:27:50 +0000

    Adding Shop files

---
+++ database/migrations/2024_11_05_001650_create_store_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        DB::statement("SET NAMES utf8mb4;
+                                INSERT INTO `store_types` (`id`, `name`, `created_at`, `updated_at`) VALUES
+                                    (1,	'Takeaway',	now(),	NULL),
+                                    (2,	'Shop',	now(),	NULL),
+                                    (3,	'Restaurant',	now(),	NULL);");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_types');
+    }
+};
---
+++ database/migrations/2024_11_05_224247_create_shops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->decimal('latitude', 9, 6)->nullable();
+            $table->decimal('longitude', 9, 6)->nullable();
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->bigInteger('store_type_id')->unsigned();
+            $table->foreign('store_type_id')->references('id')->on('store_types');
+            $table->double('max_delivery_distance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
